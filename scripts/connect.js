4056a52c93cfcfed90eea47943ca19ecb5d8c656
will ruby <me@willruby.com>
2017-04-07 15:29:10 -0700

    connect.js proof of working console.log connection bridging

--- scripts/connect.js
+++ scripts/connect.js
@@ -2,8 +2,21 @@ function Connect(options){
 	var pc = new RTCPeerConnection();
 	
 	var isHost = window.location.hash === '#host'
-	var dc = pc.createDataChannel('aaa');
-	
+	if(isHost){
+		var dc = pc.createDataChannel('a'+Math.random());
+		dc.onmessage = handleReceiveMessage
+
+		dc.onopen = function () {
+		  console.log(dc)
+		  console.log("datachannel open");
+		};
+
+	} else {
+		pc.ondatachannel = function(e){
+			console.log(e.channel)
+		    e.channel.onmessage = handleReceiveMessage;
+		}
+	}
 	var out = ""
 
 	pc.onicecandidate = e => {
@@ -27,27 +40,22 @@ function Connect(options){
 		})
 	}
 
-	dc.onmessage = function (event) {
-	  console.log("received: " + event.data);
-	};
-
-	dc.onopen = function () {
-	  console.log(dc)
-	  console.log("datachannel open");
-	};
-
-	dc.onclose = function () {
-	  console.log("datachannel close");
-	};
+	function handleReceiveMessage(event){
+		console.log("received: " + event.data);
+	}
 	
 	function giveAnswer(){
 		var p = new Promise(function(resolve,reject){
 			answer = function(str){				
 				var split = str.split("=!=!=!=")
-				resolve(JSON.parse(split[0].replace(/\r\n/g,"\\r\\n")))
+				
+				var session = JSON.parse(split[0].replace(/\r\n/g,"\\r\\n"))
+				resolve(new RTCSessionDescription(session))
+				
 				setTimeout(function(){
-					pc.addIceCandidate(JSON.parse(split[1]))
+					pc.addIceCandidate(new RTCIceCandidate(JSON.parse(split[1])))
 				})
+			
 			}
 		})
 		
